3a59d491e0fe5d8b9c228463f0141ab84721a5e8
Hussein Playz <houseinplayz@gmail.com>
2024-11-20 14:37:22 +0300

    Add files via upload

---
+++ src/main/java/com/luna/Gsons.java
@@ -0,0 +1,92 @@
+/*
+ * ATLauncher - https://github.com/ATLauncher/ATLauncher
+ * Copyright (C) 2013-2022 ATLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.luna;
+
+import java.awt.Color;
+import java.time.Instant;
+import java.util.Date;
+
+import com.luna.annot.ExcludeFromGsonSerialization;
+import com.luna.data.AbstractAccount;
+import com.luna.data.AccountTypeAdapter;
+import com.luna.data.ColorTypeAdapter;
+import com.luna.data.DateTypeAdapter;
+import com.luna.data.InstantTypeAdapter;
+import com.luna.data.PackVersion;
+import com.luna.data.PackVersionTypeAdapter;
+import com.luna.data.microsoft.OauthTokenResponse;
+import com.luna.data.microsoft.OauthTokenResponseTypeAdapter;
+import com.luna.data.minecraft.Arguments;
+import com.luna.data.minecraft.ArgumentsTypeAdapter;
+import com.luna.data.minecraft.Library;
+import com.luna.data.minecraft.LibraryTypeAdapter;
+import com.luna.data.minecraft.loaders.fabric.FabricLibrary;
+import com.luna.data.minecraft.loaders.fabric.FabricLibraryTypeAdapter;
+import com.luna.data.minecraft.loaders.fabric.FabricMetaLauncherMeta;
+import com.luna.data.minecraft.loaders.fabric.FabricMetaLauncherMetaTypeAdapter;
+import com.luna.data.minecraft.loaders.forge.ForgeLibrary;
+import com.luna.data.minecraft.loaders.forge.ForgeLibraryTypeAdapter;
+import com.luna.data.minecraft.loaders.legacyfabric.LegacyFabricLibrary;
+import com.luna.data.minecraft.loaders.legacyfabric.LegacyFabricLibraryTypeAdapter;
+import com.luna.data.minecraft.loaders.legacyfabric.LegacyFabricMetaLauncherMeta;
+import com.luna.data.minecraft.loaders.legacyfabric.LegacyFabricMetaLauncherMetaTypeAdapter;
+import com.luna.data.minecraft.loaders.quilt.QuiltLibrary;
+import com.luna.data.minecraft.loaders.quilt.QuiltLibraryTypeAdapter;
+import com.luna.data.minecraft.loaders.quilt.QuiltMetaLauncherMeta;
+import com.luna.data.minecraft.loaders.quilt.QuiltMetaLauncherMetaTypeAdapter;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public final class Gsons {
+    public static final ExclusionStrategy exclusionAnnotationStrategy = new ExclusionStrategy() {
+        @Override
+        public boolean shouldSkipClass(Class<?> clazz) {
+            return false;
+        }
+
+        @Override
+        public boolean shouldSkipField(FieldAttributes field) {
+            return field.getAnnotation(ExcludeFromGsonSerialization.class) != null;
+        }
+    };
+
+    private static final Gson BASE = new GsonBuilder()
+            .disableHtmlEscaping()
+            .registerTypeAdapter(AbstractAccount.class, new AccountTypeAdapter())
+            .registerTypeAdapter(Date.class, new DateTypeAdapter())
+            .registerTypeAdapter(Color.class, new ColorTypeAdapter())
+            .registerTypeAdapter(OauthTokenResponse.class, new OauthTokenResponseTypeAdapter())
+            .registerTypeAdapter(PackVersion.class, new PackVersionTypeAdapter())
+            .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+            .registerTypeAdapter(Library.class, new LibraryTypeAdapter())
+            .registerTypeAdapter(Arguments.class, new ArgumentsTypeAdapter())
+            .registerTypeAdapter(FabricMetaLauncherMeta.class, new FabricMetaLauncherMetaTypeAdapter())
+            .registerTypeAdapter(FabricLibrary.class, new FabricLibraryTypeAdapter())
+            .registerTypeAdapter(LegacyFabricMetaLauncherMeta.class, new LegacyFabricMetaLauncherMetaTypeAdapter())
+            .registerTypeAdapter(LegacyFabricLibrary.class, new LegacyFabricLibraryTypeAdapter())
+            .registerTypeAdapter(ForgeLibrary.class, new ForgeLibraryTypeAdapter())
+            .registerTypeAdapter(QuiltLibrary.class, new QuiltLibraryTypeAdapter())
+            .registerTypeAdapter(QuiltMetaLauncherMeta.class, new QuiltMetaLauncherMetaTypeAdapter())
+            .addSerializationExclusionStrategy(exclusionAnnotationStrategy).create();
+
+    public static final Gson DEFAULT = BASE.newBuilder().setPrettyPrinting().create();
+
+    public static final Gson DEFAULT_SLIM = BASE.newBuilder().create();
+}
---
+++ src/main/java/com/luna/themes/lunaLaf.java
@@ -0,0 +1,241 @@
+/*
+ * ATLauncher - https://github.com/ATLauncher/ATLauncher
+ * Copyright (C) 2013-2022 ATLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.luna.themes;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.Window;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JPopupMenu;
+
+import com.luna.App;
+import com.luna.data.Language;
+import com.luna.managers.LogManager;
+import com.luna.utils.Resources;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
+@SuppressWarnings("serial")
+public class lunaLaf extends FlatLaf {
+    public static lunaLaf instance;
+
+    private final String defaultFontName = "OpenSans-Regular";
+    private final String defaultBoldFontName = "OpenSans-Bold";
+    private final String consoleFontName = "OpenSans-Regular";
+    private final String tabFontName = "Oswald-Regular";
+
+    public static boolean install() {
+        instance = new lunaLaf();
+
+        return setup(instance);
+    }
+
+    public static lunaLaf getInstance() {
+        return instance;
+    }
+
+    /**
+     * If user has disabled custom fonts or is using a language without a font, then
+     * we should use the base "sansserif" font to let the OS font take over.
+     */
+    private static boolean useBaseFont() {
+        return App.settings.disableCustomFonts || Language.localesWithoutFont.contains(Language.selectedLocale);
+    }
+
+    /**
+     * If user has disabled custom fonts or is using a language without a tab font,
+     * then we should use the base "sansserif" font to let the OS font take over.
+     */
+    private static boolean useTabFont() {
+        return App.settings.disableCustomFonts || Language.localesWithoutTabFont.contains(Language.selectedLocale);
+    }
+
+    public Font getNormalFont() {
+        if (useBaseFont()) {
+            return Resources.makeFont("sansserif").deriveFont(Font.PLAIN, 12f);
+        } else {
+            return Resources.makeFont(defaultFontName).deriveFont(Font.PLAIN, 12f);
+        }
+    }
+
+    public Font getBoldFont() {
+        if (useBaseFont()) {
+            return Resources.makeFont("sansserif").deriveFont(Font.BOLD, 12f);
+        } else {
+            return Resources.makeFont(defaultFontName).deriveFont(Font.BOLD, 12f);
+        }
+    }
+
+    public Font getConsoleFont() {
+        if (useBaseFont()) {
+            return Resources.makeFont("sansserif").deriveFont(Font.PLAIN, 12f);
+        } else {
+            return Resources.makeFont(consoleFontName).deriveFont(Font.PLAIN, 12f);
+        }
+    }
+
+    public Font getTabFont() {
+        if (useTabFont()) {
+            return Resources.makeFont("sansserif").deriveFont(Font.PLAIN, 32f);
+        } else {
+            return Resources.makeFont(tabFontName).deriveFont(Font.PLAIN, 32f);
+        }
+    }
+
+    public void registerFonts() {
+        try {
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Resources.makeFont(defaultFontName));
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Resources.makeFont(defaultBoldFontName));
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Resources.makeFont(consoleFontName));
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Resources.makeFont(tabFontName));
+        } catch (Throwable t) {
+            LogManager.logStackTrace("Error registering fonts", t);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "luna";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Default theme of luna";
+    }
+
+    @Override
+    public boolean isDark() {
+        return true;
+    }
+
+    public boolean isIntelliJTheme() {
+        return false;
+    }
+
+    @Override
+    public List<Class<?>> getLafClassesForDefaultsLoading() {
+        List<Class<?>> classes = new ArrayList<>();
+
+        classes.add(FlatLaf.class); // FlatLaf class
+
+        // Add the themes base dark/light class
+        if (isDark()) {
+            classes.add(FlatDarkLaf.class);
+
+            if (isIntelliJTheme()) {
+                classes.add(FlatDarculaLaf.class);
+            }
+        } else {
+            classes.add(FlatLightLaf.class);
+
+            if (isIntelliJTheme()) {
+                classes.add(FlatIntelliJLaf.class);
+            }
+        }
+
+        classes.add(lunaLaf.class); // luna base class
+
+        if (getClass().getSuperclass() != lunaLaf.class) {
+            classes.add(getClass().getSuperclass()); // Dark/Light luna base class
+        }
+
+        classes.add(getClass()); // Theme's class
+
+        return classes;
+    }
+
+    public String getIconPath(String icon) {
+        // check for a theme specific icon first
+        String themeSpecificPath = "/assets/icon/" + (isDark() ? "dark" : "light") + "/" + icon + ".png";
+        if (App.class.getResource(themeSpecificPath) != null) {
+            return themeSpecificPath;
+        }
+
+        // if no theme specific icon, then return path to where a general one should be
+        return "/assets/icon/" + icon + ".png";
+    }
+
+    public String getResourcePath(String path, String icon) {
+        // check for a theme specific icon first
+        String themeSpecificPath = "/assets/" + path + "/" + (isDark() ? "dark" : "light") + "/" + icon + ".png";
+        if (App.class.getResource(themeSpecificPath) != null) {
+            return themeSpecificPath;
+        }
+
+        // if no theme specific icon, then return path to where a general one should be
+        return "/assets/" + path + "/" + icon + ".png";
+    }
+
+    public void updateUIFonts() {
+        EventQueue.invokeLater(() -> {
+            for (Window w : Window.getWindows()) {
+                updateFontInComponentTree(w);
+            }
+        });
+    }
+
+    private void updateFontInComponentTree(Component c) {
+        if (c instanceof JComponent) {
+            JComponent jc = (JComponent) c;
+            JPopupMenu jpm = jc.getComponentPopupMenu();
+            if (jpm != null) {
+                updateFontInComponentTree(jpm);
+            }
+        }
+        Component[] children = null;
+        if (c instanceof JMenu) {
+            children = ((JMenu) c).getMenuComponents();
+        } else if (c instanceof Container) {
+            children = ((Container) c).getComponents();
+        }
+        if (children != null) {
+            for (Component child : children) {
+                updateFontInComponentTree(child);
+            }
+        }
+
+        Font f = c.getFont();
+        if (f != null) {
+            Font newFont = App.THEME.getNormalFont();
+
+            if (f.isBold()) {
+                newFont = App.THEME.getBoldFont();
+            }
+
+            if (f.getSize() == 32f) {
+                newFont = App.THEME.getTabFont();
+            } else if (f.getSize() == 17f) {
+                newFont = App.THEME.getNormalFont().deriveFont(17.0F);
+            } else {
+                newFont = newFont.deriveFont(f.getSize());
+            }
+
+            c.setFont(newFont);
+        }
+    }
+}
