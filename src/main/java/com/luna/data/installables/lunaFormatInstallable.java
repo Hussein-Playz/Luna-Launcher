0bc187796302b4af1151a96c8e9f534a796f5647
Hussein Playz <houseinplayz@gmail.com>
2024-11-20 14:45:56 +0300

    Add files via upload

---
+++ src/main/java/com/luna/data/installables/lunaFormatInstallable.java
@@ -0,0 +1,54 @@
+/*
+ * ATLauncher - https://github.com/ATLauncher/ATLauncher
+ * Copyright (C) 2013-2022 ATLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.luna.data.installables;
+
+import com.luna.data.Pack;
+import com.luna.data.PackVersion;
+import com.luna.data.minecraft.loaders.LoaderVersion;
+
+public abstract class lunaFormatInstallable extends Installable {
+    public Pack pack;
+    public PackVersion packVersion;
+    public LoaderVersion loaderVersion;
+
+    public lunaFormatInstallable() {
+        this(null, null, null);
+    }
+
+    public lunaFormatInstallable(Pack pack, PackVersion packVersion, LoaderVersion loaderVersion) {
+        this.pack = pack;
+        this.packVersion = packVersion;
+        this.loaderVersion = loaderVersion;
+    }
+
+    @Override
+    public Pack getPack() {
+        return this.pack;
+    }
+
+    @Override
+    public PackVersion getPackVersion() {
+        return this.packVersion;
+    }
+
+    @Override
+    public LoaderVersion getLoaderVersion() {
+        return this.loaderVersion;
+    }
+
+}
